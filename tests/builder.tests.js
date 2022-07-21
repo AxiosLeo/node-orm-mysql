92beaa304b403d3b8a5337ec2a790f21bf0ccaca
axiosleo <axiosleo@foxmail.com>
2022-07-21 13:18:37 +0800

    test: builder test case

---
+++ tests/builder.tests.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const expect = require('chai').expect;
+const { buildSql } = require('../src/builder');
+
+describe('builder test case', () => {
+  it('select table should be ok', () => {
+    const options = {
+      sql: '',
+      values: [],
+      conditions: [],
+      orders: [],
+      tables: [],
+      operator: null,
+      data: null,
+      groupField: [],
+    };
+    options.tables.push({ tableName: 'table1', alias: 't1' });
+    options.operator = 'select';
+    expect(buildSql(options).sql).to.be.equal('SELECT * FROM `table1` AS `t1`');
+  });
+
+  it('where should be ok', () => {
+    const options = {
+      sql: '',
+      values: [],
+      conditions: [],
+      orders: [],
+      tables: [{ tableName: 'table1', alias: 't1' }],
+      operator: 'select',
+      data: null,
+      groupField: [],
+    };
+    options.conditions.push({
+      key: 't1.id',
+      opt: '=',
+      value: 1,
+    }, {
+      key: null,
+      opt: 'AND',
+      value: 1,
+    }, {
+      key: null,
+      opt: 'OR',
+      value: null
+    }, {
+      key: 'id',
+      opt: '>',
+      value: 1,
+    });
+    expect(buildSql(options).sql).to.be.equal('SELECT * FROM `table1` AS `t1` WHERE `t1`.`id` = ? AND ? OR `id` > ?');
+  });
+});
