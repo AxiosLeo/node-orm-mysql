aafee97d432ff22db2bbbd9f9514c7373d8566a7
axiosleo <axiosleo@foxmail.com>
2022-08-01 14:18:22 +0800

    feat: supported sub query

--- src/query.js
+++ src/query.js
@@ -1,38 +1,17 @@
 'use strict';
 
-const { buildSql } = require('./builder');
-
-const query = async (conn, options) => {
-  return new Promise((resolve, reject) => {
-    conn.query(options, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-};
-
-class QueryOperator {
-  /**
-   * 
-   * @param {*} conn 
-   * @param {TableOption} table 
-   */
-  constructor(conn, table) {
-    this.conn = conn;
+class Query {
+  constructor(operator = 'select') {
     this.options = {
       sql: '',
       values: [],
       conditions: [],
       orders: [],
       tables: [],
-      operator: null,
+      operator,
       data: null,
       groupField: [],
     };
-    this.options.tables.push(table);
   }
 
   table(tableName, alias) {
@@ -135,108 +114,6 @@ class QueryOperator {
     this.options.joins.push({ table, alias, on, type });
     return this;
   }
-
-  buildSql(operator) {
-    this.options.operator = operator;
-    return buildSql(this.options);
-  }
-
-  async exec() {
-    if (!this.options.operator) {
-      throw new Error('Invalid operator: ' + this.options.operator);
-    }
-    const { sql, values } = this.buildSql(this.options.operator);
-    const options = {
-      sql, values
-    };
-    switch (this.options.operator) {
-      case 'find': {
-        const res = await query(this.conn, options);
-        return res[0];
-      }
-      case 'count': {
-        const [res] = await query(this.conn, options);
-        return res.count;
-      }
-      default:
-        return query(this.conn, options);
-    }
-  }
-
-  async select() {
-    this.options.operator = 'select';
-    return await this.exec();
-  }
-
-  async find() {
-    this.options.operator = 'find';
-    return await this.exec();
-  }
-
-  async update(data) {
-    this.options.operator = 'update';
-    if (data) {
-      this.set(data);
-    }
-    return await this.exec();
-  }
-
-  async insert(data) {
-    this.options.operator = 'insert';
-    if (data) {
-      this.set(data);
-    }
-    return await this.exec();
-  }
-
-  async count() {
-    this.options.operator = 'count';
-    return await this.exec();
-  }
-
-  async delete(id) {
-    if (id) {
-      this.where('id', id);
-    }
-    this.options.operator = 'delete';
-    return await this.exec();
-  }
 }
 
-class QueryHandler {
-  constructor(conn) {
-    this.conn = conn;
-  }
-
-  async query(options) {
-    return new Promise((resolve, reject) => {
-      this.conn.query(options, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  }
-
-  table(table, alias = null) {
-    return new QueryOperator(this.conn, {
-      tableName: table,
-      alias
-    });
-  }
-
-  async upsert(tableName, data, condition = {}) {
-    const count = await this.table(tableName).whereObject(condition).count();
-    if (count) {
-      return await this.table(tableName).whereObject(condition).update(data);
-    }
-    return await this.table(tableName).insert(data);
-  }
-}
-
-module.exports = {
-  QueryOperator,
-  QueryHandler
-};
+module.exports = Query;
