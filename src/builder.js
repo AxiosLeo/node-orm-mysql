fa4ee8f589a7c789e5982db2b270cfcd3b22ba2a
axiosleo <axiosleo@foxmail.com>
2022-08-06 11:31:55 +0800

    refactor: builder

--- src/builder.js
+++ src/builder.js
@@ -1,201 +1,191 @@
 'use strict';
 
+const Query = require('./query');
 const is = require('@axiosleo/cli-tool/src/helper/is');
 
-const _buildFieldWithTableName = (key) => {
-  if (key.indexOf('$') !== -1) {
-    return key;
-  }
-  return key.split('.').map((k) => k.indexOf('`') !== -1 ? k : `\`${k}\``).join('.');
-};
+class Builder {
+  constructor(options) {
+    let sql = options.sql;
+    this.values = options.values || [];
 
-const _buildFieldKey = (key) => {
-  if (key === null) {
-    return '';
-  }
-  if (key.indexOf('(') !== -1 && key.indexOf(')') !== -1) {
-    let field = key.substring(key.indexOf('(') + 1, key.indexOf(')'));
-    key = key.substring(0, key.indexOf('(')) + '(' + _buildFieldWithTableName(field) + ')' + key.substring(key.indexOf(')') + 1);
-  }
-  if (key.indexOf(' as ') !== -1) {
-    const field = key.substring(key.indexOf(' as ') + 4);
-    key = key.substring(0, key.indexOf(' as ')) + ' AS ' + _buildFieldWithTableName(field);
-  } else if (key.indexOf(' AS ') !== -1) {
-    const field = key.substring(key.indexOf(' AS ') + 4);
-    key = key.substring(0, key.indexOf(' AS ')) + ' AS ' + _buildFieldWithTableName(field);
-  }
-  return _buildFieldWithTableName(key);
-};
-
-const _buildContidion = (conditions, prefix) => {
-  const values = [];
-  if (is.empty(conditions)) {
-    return { sql: '', values };
-  }
-  let sql = typeof prefix === 'undefined' ? ' WHERE ' : '';
-  if (conditions.length) {
-    sql += `${conditions.map((c) => {
-      if (c.key === null && c.value === null) {
-        return ` ${c.opt} `;
+    switch (options.operator) {
+      case 'find': {
+        options.pageLimit = 1;
+        options.pageOffset = 0;
       }
-      if (c.value === null) {
-        return c.opt === '=' ? `ISNULL(${_buildFieldKey(c.key)})` : `!ISNULL(${_buildFieldKey(c.key)})`;
+      // eslint-disable-next-line no-fallthrough
+      case 'select': {
+        sql = `SELECT ${options.attrs ? options.attrs.map((a) => this._buildFieldKey(a)).join(',') : '*'} FROM ${this._buildTables(options.tables)}`;
+        sql += this._buildJoins(options.joins);
+        sql += this._buildContidion(options.conditions);
+        sql += options.orders.length > 0 ? this._buildOrders(options.orders) : '';
+        sql += this._buldPagenation(options.pageLimit, options.pageOffset);
+        if (options.groupField.length) {
+          sql += ` GROUP BY ${options.groupField.join(',')}`;
+        }
+        break;
       }
-      if (c.key && c.key.indexOf('->') !== -1) {
-        const keys = c.key.split('->');
-        values.push(c.value);
-        const res = _buildContidion([
-          {
-            key: `JSON_EXTRACT(${_buildFieldKey(keys[0])}, '${keys[1]}')`,
-            opt: c.opt,
-            value: c.value
-          }
-        ], '');
-        values.push(...res.values);
-        return res.sql;
+      case 'insert': {
+        const fields = this._buildValues(options.data);
+        sql = `INSERT INTO ${this._buildTables(options.tables)}(${fields.map((f) => this._buildFieldKey(f))}) VALUES (${fields.map(() => '?').join(',')})`;
+        break;
       }
-      const opt = c.opt.toLowerCase();
-      if (opt === 'in' && Array.isArray(c.value)) {
-        values.push(c.value.join(','));
-        return `${_buildFieldKey(c.key)} IN (?)`;
-      } else if (opt === 'group' && Array.isArray(c.value)) {
-        const res = _buildContidion(c.value, '');
-        values.push(...res.values);
-        return `(${res.sql})`;
+      case 'update': {
+        const fields = this._buildValues(options.data);
+        sql = `UPDATE ${this._buildTables(options.tables)} SET ${fields.map(f => `${this._buildFieldKey(f)} = ?`).join(',')}`;
+        sql += this._buildContidion(options.conditions);
+        break;
       }
-      values.push(c.value);
-      return `${_buildFieldKey(c.key)} ${c.opt} ?`;
-    }).join('')}`;
-  }
-  return {
-    sql,
-    values
-  };
-};
-
-const _buildValues = (obj) => {
-  const fields = [];
-  const values = [];
-  Object.keys(obj).forEach((key) => {
-    fields.push(`${key}`);
-    if (obj[key] instanceof Date) {
-      values.push(obj[key]);
-    } else if (Array.isArray(obj[key]) || is.object(obj[key])) {
-      values.push(JSON.stringify(obj[key]));
-    } else {
-      values.push(obj[key]);
-    }
-  });
-  return { fields, values };
-};
-
-const _buldPagenation = (limit, offset) => {
-  let sql = '';
-  if (limit) {
-    sql += ` LIMIT ${limit}`;
-  }
-  if (offset) {
-    sql += ` OFFSET ${offset}`;
-  }
-  return sql;
-};
-
-const _buildTables = (tables) => {
-  return tables.map((t) => {
-    if (t.alias) {
-      return `\`${t.tableName}\` AS \`${t.alias}\``;
-    }
-    return `\`${t.tableName}\``;
-  }).join(' , ');
-};
-
-const _buildOrders = (orders = []) => {
-  const sql = ' ORDER BY ' + orders.map((o) => {
-    return `\`${o.sortField}\` ${o.sortOrder}`;
-  }).join(',');
-  return sql;
-};
-
-const _buildJoins = (joins = []) => {
-  return joins.map((j) => {
-    switch (j.type) {
-      case 'left':
-        return ` LEFT JOIN \`${j.table}\` AS \`${j.alias}\` ON ${j.on}`;
-      case 'inner':
-        return ` INNER JOIN \`${j.table}\` AS \`${j.alias}\` ON ${j.on}`;
-      case 'right':
-        return ` RIGHT JOIN \`${j.table}\` AS \`${j.alias}\` ON ${j.on}`;
-    }
-  }).join(' ');
-};
-
-const buildSql = (options) => {
-  let sql = options.sql;
-  let values = options.values;
-  switch (options.operator) {
-    case 'find': {
-      options.pageLimit = 1;
-      options.pageOffset = 0;
-    }
-    // eslint-disable-next-line no-fallthrough
-    case 'select': {
-      sql = `SELECT ${options.attrs ? options.attrs.map((a) => _buildFieldKey(a)).join(',') : '*'} FROM ${_buildTables(options.tables)}`;
-      sql += _buildJoins(options.joins);
-      const res = _buildContidion(options.conditions);
-      sql += res.sql;
-      values = options.values.concat(res.values);
-      sql += options.orders.length > 0 ? _buildOrders(options.orders) : '';
-      sql += _buldPagenation(options.pageLimit, options.pageOffset);
-      if (options.groupField.length) {
-        sql += ` GROUP BY ${options.groupField.join(',')}`;
+      case 'delete': {
+        sql = `DELETE FROM ${this._buildTables(options.tables)}`;
+        if (!options.conditions.length) {
+          throw new Error('At least one where condition is required for delete operation');
+        }
+        sql += this._buildContidion(options.conditions);
+        break;
       }
-      break;
-    }
-    case 'insert': {
-      const buildValueRes = _buildValues(options.data);
-      sql = `INSERT INTO ${_buildTables(options.tables)}(${buildValueRes.fields.map((f) => _buildFieldKey(f))}) VALUES (${buildValueRes.fields.map(() => '?').join(',')})`;
-      values = values.concat(buildValueRes.values);
-      break;
-    }
-    case 'update': {
-      const buildValueRes = _buildValues(options.data);
-      sql = `UPDATE ${_buildTables(options.tables)} SET ${buildValueRes.fields.map(f => `${_buildFieldKey(f)} = ?`).join(',')}`;
-      values = values.concat(buildValueRes.values);
-      const buildConditionRes = _buildContidion(options.conditions);
-      sql += buildConditionRes.sql;
-      values = values.concat(buildConditionRes.values);
-      break;
-    }
-    case 'delete': {
-      sql = `DELETE FROM ${_buildTables(options.tables)}`;
-      if (!options.conditions.length) {
-        throw new Error('At least one where condition is required for delete operation');
+      case 'count': {
+        sql = `SELECT COUNT(*) AS count FROM ${this._buildTables(options.tables)}`;
+        sql += this._buildContidion(options.conditions);
+        if (options.groupField.length) {
+          sql += ` GROUP BY ${options.groupField.join(',')}`;
+        }
+        break;
       }
-      const buildConditionRes = _buildContidion(options.conditions);
-      sql += buildConditionRes.sql;
-      values = values.concat(buildConditionRes.values);
-      break;
+      default:
+        throw new Error('Invalid operator: ' + options.operator);
     }
-    case 'count': {
-      sql = `SELECT COUNT(*) AS count FROM ${_buildTables(options.tables)}`;
-      const buildConditionRes = _buildContidion(options.conditions);
-      sql += buildConditionRes.sql;
-      values = values.concat(buildConditionRes.values);
-      if (options.groupField.length) {
-        sql += ` GROUP BY ${options.groupField.join(',')}`;
-      }
-      break;
-    }
-    default:
-      throw new Error('Invalid operator: ' + options.operator);
+
+    this.sql = sql;
   }
 
-  return {
-    sql,
-    values: values
-  };
-};
+  _buildJoins(joins = []) {
+    return joins.map((j) => {
+      switch (j.type) {
+        case 'left':
+          return ` LEFT JOIN \`${j.table}\` AS \`${j.alias}\` ON ${j.on}`;
+        case 'inner':
+          return ` INNER JOIN \`${j.table}\` AS \`${j.alias}\` ON ${j.on}`;
+        case 'right':
+          return ` RIGHT JOIN \`${j.table}\` AS \`${j.alias}\` ON ${j.on}`;
+      }
+    }).join(' ');
+  }
+
+  _buildOrders(orders = []) {
+    const sql = ' ORDER BY ' + orders.map((o) => {
+      return `\`${o.sortField}\` ${o.sortOrder}`;
+    }).join(',');
+    return sql;
+  }
+
+  _buildTables(tables) {
+    return tables.map((t) => {
+      if (t.alias) {
+        return `\`${t.tableName}\` AS \`${t.alias}\``;
+      }
+      return `\`${t.tableName}\``;
+    }).join(' , ');
+  }
+
+  _buldPagenation(limit, offset) {
+    let sql = '';
+    if (limit) {
+      sql += ` LIMIT ${limit}`;
+    }
+    if (offset) {
+      sql += ` OFFSET ${offset}`;
+    }
+    return sql;
+  }
+
+  _buildValues(obj) {
+    const fields = [];
+    Object.keys(obj).forEach((key) => {
+      fields.push(`${key}`);
+      if (obj[key] instanceof Date) {
+        this.values.push(obj[key]);
+      } else if (Array.isArray(obj[key]) || is.object(obj[key])) {
+        this.values.push(JSON.stringify(obj[key]));
+      } else {
+        this.values.push(obj[key]);
+      }
+    });
+    return fields;
+  }
+
+  _buildConditionValues(val) {
+    if (val instanceof Query) {
+      const builder = new Builder(val.options);
+      this.values = this.values.concat(builder.values);
+      return builder.sql;
+    }
+    return null;
+  }
+
+  _buildContidion(conditions, prefix) {
+    if (is.empty(conditions)) {
+      return '';
+    }
+    let sql = typeof prefix === 'undefined' ? ' WHERE ' : '';
+    if (conditions.length) {
+      sql += `${conditions.map((c) => {
+        if (c.key === null && c.value === null) {
+          return ` ${c.opt} `;
+        }
+        if (c.value === null) {
+          return c.opt === '=' ? `ISNULL(${this._buildFieldKey(c.key)})` : `!ISNULL(${this._buildFieldKey(c.key)})`;
+        }
+        if (c.key && c.key.indexOf('->') !== -1) {
+          const keys = c.key.split('->');
+          return this._buildContidion([
+            {
+              key: `JSON_EXTRACT(${this._buildFieldKey(keys[0])}, '${keys[1]}')`,
+              opt: c.opt,
+              value: c.value
+            }
+          ], '');
+        }
+        const opt = c.opt.toLowerCase();
+        if (opt === 'in' && Array.isArray(c.value)) {
+          let res = this._buildConditionValues(c.value);
+          return res ? `${this._buildFieldKey(c.key)} IN (${res})` : `${this._buildFieldKey(c.key)} IN (?)`;
+        } else if (opt === 'group' && Array.isArray(c.value)) {
+          return `(${this._buildContidion(c.value, '')})`;
+        }
+        let res = this._buildConditionValues(c.value);
+        return res ? `${this._buildFieldKey(c.key)} ${c.opt} (${res})` : `${this._buildFieldKey(c.key)} ${c.opt} ?`;
+      }).join('')}`;
+    }
+    return sql;
+  }
+
+  _buildFieldKey(key) {
+    if (key === null) {
+      return '';
+    }
+    if (key.indexOf('(') !== -1 && key.indexOf(')') !== -1) {
+      let field = key.substring(key.indexOf('(') + 1, key.indexOf(')'));
+      key = key.substring(0, key.indexOf('(')) + '(' + this._buildFieldWithTableName(field) + ')' + key.substring(key.indexOf(')') + 1);
+    }
+    if (key.indexOf(' as ') !== -1) {
+      const field = key.substring(key.indexOf(' as ') + 4);
+      key = key.substring(0, key.indexOf(' as ')) + ' AS ' + this._buildFieldWithTableName(field);
+    } else if (key.indexOf(' AS ') !== -1) {
+      const field = key.substring(key.indexOf(' AS ') + 4);
+      key = key.substring(0, key.indexOf(' AS ')) + ' AS ' + this._buildFieldWithTableName(field);
+    }
+    return this._buildFieldWithTableName(key);
+  }
+
+  _buildFieldWithTableName(key) {
+    if (key.indexOf('$') !== -1) {
+      return key;
+    }
+    return key.split('.').map((k) => k.indexOf('`') !== -1 ? k : `\`${k}\``).join('.');
+  }
+}
 
 module.exports = {
-  buildSql
+  Builder
 };
