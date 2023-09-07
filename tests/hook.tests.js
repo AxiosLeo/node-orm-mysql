a4a6e48f76a44d47e0a9b99451692a413c2f0a1e
axiosleo <axiosleo@foxmail.com>
2023-09-07 15:55:09 +0800

    refactor: hook

--- tests/hook.tests.js
+++ tests/hook.tests.js
@@ -1,16 +1,36 @@
 'use strict';
 
 const expect = require('chai').expect;
-const { Hook } = require('../src/hook');
+const Hook = require('../src/hook');
 
 describe('hook test case', () => {
-  it('push', () => {
-    const callback = () => { };
+  it('pre', () => {
+    const callback = (...args) => {
+      expect(true).to.be.true;
+      expect(args).to.be.an('array');
+    };
     const event = Hook.pre(callback, { table: 'test', opt: 'test' });
     expect(event).to.be.an('object');
     expect(event.label).to.equal('pre');
     expect(event.table).to.equal('test');
     expect(event.opt).to.equal('test');
     expect(event.callback).to.equal(callback);
+
+    Hook.trigger('pre', { table: 'test', opt: 'test' }, 1, 2, 3);
+  });
+
+  it('post', () => {
+    const callback = (...args) => {
+      expect(true).to.be.true;
+      expect(args).to.be.an('array');
+    };
+    const event = Hook.post(callback, { table: 'test', opt: 'test' });
+    expect(event).to.be.an('object');
+    expect(event.label).to.equal('post');
+    expect(event.table).to.equal('test');
+    expect(event.opt).to.equal('test');
+    expect(event.callback).to.equal(callback);
+
+    Hook.trigger('post', { table: 'test', opt: 'test' }, 1, 2, 3);
   });
 });
