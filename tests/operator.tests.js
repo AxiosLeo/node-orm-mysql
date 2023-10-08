ea8509e48ee7015abf25db3205fb27da4dd486dc
axiosleo <axiosleo@foxmail.com>
2023-10-08 19:38:28 +0800

    test: add transaction test case

--- tests/operator.tests.js
+++ tests/operator.tests.js
@@ -3,6 +3,7 @@
 const expect = require('chai').expect;
 const { Builder } = require('../src/builder');
 const { QueryOperator } = require('../src/operator');
+const { TransactionHandler } = require('../src/transaction');
 
 describe('operator test case', () => {
   it('find', async () => {
@@ -37,5 +38,44 @@ describe('operator test case', () => {
     expect(res).to.be.an('array');
     expect(res[0].a).to.be.equal(1);
     expect(res[0].b).to.be.equal(2);
+  });
+
+  it('transaction', async () => {
+    const conn = {
+      execute: async (sql, values) => {
+        return [{ insertId: 1 }];
+      },
+      beginTransaction: async () => { },
+      rollback: async () => { },
+      commit: async () => { },
+    };
+    const transaction = new TransactionHandler(conn, {
+      /*
+      level = 'READ UNCOMMITTED' | 'RU'
+            | 'READ COMMITTED' | 'RC'
+            | 'REPEATABLE READ' | 'RR'
+            | 'SERIALIZABLE' | 'S'
+      */
+      level: 'SERIALIZABLE', // 'SERIALIZABLE' as default value
+    });
+    await transaction.begin();
+    try {
+      // insert user info
+      // will not really create a record.
+      let row = await transaction.table('users').insert({
+        name: 'Joe',
+        age: 18,
+      });
+      const lastInsertId = row[0].insertId;
+
+      // insert student info
+      await transaction.table('students').insert({
+        user_id: lastInsertId,
+      });
+      await transaction.commit();
+    } catch (e) {
+      await transaction.rollback();
+      throw e;
+    }
   });
 });
