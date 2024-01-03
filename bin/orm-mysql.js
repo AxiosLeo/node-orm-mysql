cc7dfef22596f13bfabe635b92963b70c1c78272
axiosleo <axiosleo@foxmail.com>
2024-01-03 19:43:50 +0800

    feat: migration

---
+++ bin/orm-mysql.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const { App } = require('@axiosleo/cli-tool');
+
+const app = new App({
+  name: 'MySQL ORM CLI',
+  desc: 'migrate, model, seed, etc.',
+  bin: 'orm-mysql',
+  version: '0.8.6',
+  commands_dir: path.join(__dirname, '../commands'),
+});
+
+app.start();
