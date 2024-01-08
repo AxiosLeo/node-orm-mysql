2778b1e38c8c42fca25fb91ddf568828dac0c008
axiosleo <axiosleo@foxmail.com>
2024-01-08 19:09:31 +0800

    perf: _validate method

--- src/utils.js
+++ src/utils.js
@@ -3,14 +3,18 @@
 const Validator = require('validatorjs');
 const { Builder } = require('./builder');
 
-const _validate = (obj, rules) => {
+const _validate = (obj, rules, throwError = true) => {
   let validation = new Validator(obj, rules);
   validation.check();
   if (validation.fails()) {
     const errors = validation.errors.all();
     const keys = Object.keys(errors);
-    throw new Error(`${keys[0]}: ${errors[keys[0]]}`);
+    if (throwError) {
+      throw new Error(`${keys[0]}: ${errors[keys[0]]}`);
+    }
+    return errors;
   }
+  return null;
 };
 
 const _execSQL = (conn, sql, values = []) => {
