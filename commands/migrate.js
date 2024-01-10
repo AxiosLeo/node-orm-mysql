9b03e9291c4009d73b1649f96824ce5444f1f433
axiosleo <axiosleo@foxmail.com>
2024-01-10 15:55:57 +0800

    feat: Add debug option to MigrateCommand class

--- commands/migrate.js
+++ commands/migrate.js
@@ -12,14 +12,13 @@ class MigrateCommand extends Command {
     });
     this.addArgument('action', 'up or down', 'required');
     this.addArgument('dir', 'migration directory', 'optional', process.cwd());
+
+    this.addOption('debug', 'd', '[false] debug mode', 'optional', false);
     this.addOption('host', null, '[localhost] mysql host', 'optional', 'localhost');
     this.addOption('user', null, '[root] username for connect to the database', 'optional', 'root');
     this.addOption('pass', null, 'password to connect to the database', 'optional', '');
     this.addOption('port', null, '[3306] port number to connect to the database', 'optional', 3306);
     this.addOption('db', null, 'database name', 'optional', '');
-    this.addOption('debug', 'd', '[false] debug mode', 'optional', false);
-
-    this.queries = [];
   }
 
   /**
@@ -41,11 +40,16 @@ class MigrateCommand extends Command {
           user: options.user,
           password: options.pass,
           database: options.db
-        }
+        },
+        debug: options.debug
       });
       process.exit(0);
     } catch (e) {
-      debug.error(e);
+      if (e.curr && e.curr.error) {
+        debug.error(e.curr.error);
+      } else {
+        debug.log(e);
+      }
       process.exit(1);
     }
   }
