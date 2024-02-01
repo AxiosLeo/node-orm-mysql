0e9e8cdb0e56d38c358967154a74c7f2e2ae8731
axiosleo <axiosleo@foxmail.com>
2024-02-01 18:34:00 +0800

    refactor: migration

--- src/migration.js
+++ src/migration.js
@@ -1,155 +1,46 @@
 'use strict';
 
-const os = require('os');
 const path = require('path');
 const { _list, _exists } = require('@axiosleo/cli-tool/src/helper/fs');
 const { createClient, createPool, createPromiseClient } = require('./client');
 const { QueryHandler } = require('./operator');
-// eslint-disable-next-line no-unused-vars
 const { printer, debug } = require('@axiosleo/cli-tool');
-const { _execSQL, _validate } = require('./utils');
-const { _render, _caml_case } = require('@axiosleo/cli-tool/src/helper/str');
-const is = require('@axiosleo/cli-tool/src/helper/is');
+const { _execSQL } = require('./core');
+const { _render } = require('@axiosleo/cli-tool/src/helper/str');
 const { _foreach } = require('@axiosleo/cli-tool/src/helper/cmd');
 const { _assign } = require('@axiosleo/cli-tool/src/helper/obj');
 const { TransactionHandler } = require('..');
-
-const actions = {
-  // database: {
-  //   create: 'CREATE DATABASE `${database_name}` CHARACTER SET ${charset} COLLATE ${collate}',
-  //   drop: 'DROP DATABASE `${database_name}`',
-  // },
-  table: {
-    create: 'CREATE TABLE `${table_name}` (' + os.EOL +
-      '${columns}' + os.EOL +
-      ') ENGINE=${engine} DEFAULT CHARSET=utf8mb4',
-    drop: 'DROP TABLE `${table_name}`',
-  },
-  column: {
-    create: 'ALTER TABLE `${table_name}` ADD COLUMN',
-    drop: 'ALTER TABLE `${table_name}` DROP COLUMN'
-  },
-  index: {
-    create: 'CREATE INDEX `${index_name}` ON `${table_name}` (${column_names})',
-    drop: 'DROP INDEX `${index_name}`'
-  },
-  foreign_key: {
-    create: 'ALTER TABLE `${table_name}` ADD CONSTRAINT `${foreign_key_name}` FOREIGN KEY (`${column_name}`) REFERENCES `${foreign_table_name}` (`${foreign_column_name}`)',
-    drop: 'ALTER TABLE `${table_name}` DROP FOREIGN KEY `${foreign_key_name}`'
-  }
-};
+const { ManageSQLBuilder } = require('./builder');
 
 const migrationColumns = [
   {
     name: 'id',
     type: 'int',
     length: 11,
-    not_null: true,
-    auto_increment: true,
-    is_primary_key: true,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
   },
   {
     name: 'migration_key',
     type: 'varchar',
     length: 255,
-    not_null: true,
+    allowNull: false,
   },
   {
     name: 'filename',
     type: 'varchar',
     length: 255,
-    not_null: true,
-    is_uniq_index: true,
+    allowNull: false,
+    uniqIndex: true,
   },
   {
     name: 'created_at',
     type: 'datetime',
-    not_null: true,
+    allowNull: false,
     default: 'timestamp',
   }
 ];
-
-function _throwError(msg) {
-  const e = new Error(msg);
-  let tmp = e.stack.split('\n').find(i => i.indexOf('at Object.up') > -1);
-  let index = tmp.indexOf('(');
-  let local = tmp.substring(index + 1, tmp.length - 2);
-  printer.println('').yellow('at ' + local.trim()).println().println().print('    ').red(e.message).println();
-  process.exit(1);
-}
-
-function __validate(obj, rules = {}) {
-  try {
-    _validate(obj, rules);
-  } catch (e) {
-    _throwError(e.message);
-  }
-}
-
-function _renderColumn(column) {
-  __validate(column, {
-    name: 'required|string',
-    type: 'required|string',
-    default: 'string',
-    comment: 'string',
-    not_null: 'boolean',
-    auto_increment: 'boolean',
-    collate: 'string',
-  });
-  let str = `\`${column.name}\` ${column.type.toUpperCase()}`;
-  if (typeof column.length !== 'undefined') {
-    str += `(${column.length})`;
-  }
-  if (column.not_null === true) {
-    str += ' NOT NULL';
-  }
-  if (column.unsigned === true) {
-    str += ' UNSIGNED';
-  }
-  if (typeof column.default !== 'undefined') {
-    if (column.is_primary_key === true) {
-      _throwError('Primary key can not have default value.');
-    }
-    if (column.default === null) {
-      str += ' DEFAULT NULL';
-    } else if (column.default === 'timestamp') {
-      str += ' DEFAULT CURRENT_TIMESTAMP';
-    } else if (is.string(column.default)) {
-      str += ` DEFAULT ${column.default}`;
-    }
-  }
-  if (is.string(column.comment) && is.empty(column.comment) === false) {
-    str += ` COMMENT '${column.comment}'`;
-  }
-  if (column.auto_increment === true) {
-    str += ' AUTO_INCREMENT';
-  }
-  return str;
-}
-
-function _renderColumns(columns) {
-  let primary_column = null;
-  let indexs = [];
-  let strs = columns.map(column => {
-    let str = _renderColumn(column);
-    if (column.is_primary_key === true) {
-      primary_column = column;
-    } else if (column.is_uniq_index === true) {
-      indexs.push(column);
-    }
-    return str;
-  });
-  if (primary_column) {
-    strs.push(`PRIMARY KEY (\`${primary_column.name}\`)`);
-    strs.push(`UNIQUE INDEX \`${primary_column.name}\` (\`${primary_column.name}\` ASC) VISIBLE`);
-  }
-  if (indexs.length > 0) {
-    indexs.forEach((i) => {
-      strs.push(`UNIQUE INDEX \`${i.name}\` (\`${i.name}\` ASC) VISIBLE`);
-    });
-  }
-  return strs.join(',' + os.EOL);
-}
 
 /**
  * initialize migration
@@ -161,7 +52,6 @@ async function init(context) {
     files = files.reverse();
   }
   context.files = files.filter(f => f !== '.connect.js');
-  context.items = Object.keys(actions);
 
   const connectPath = path.join(context.config.dir, '.connect.js');
   if (await _exists(connectPath)) {
@@ -196,164 +86,17 @@ async function init(context) {
 
   // migration table not exists
   printer.yellow('will create table ' + context.task_key).println();
-  let res = await _execSQL(conn, _render(actions.table.create, {
-    table_name: context.task_key,
-    columns: _renderColumns(migrationColumns),
-    engine: 'InnoDB',
-  }));
+  let builder = new ManageSQLBuilder({
+    operator: 'create',
+    target: 'table',
+    name: context.task_key,
+    columns: migrationColumns
+  });
+  let res = await _execSQL(conn, builder.sql);
   conn.end();
   if (res.serverStatus !== 2) {
     printer.error('create migration table failed.');
     process.exit(1);
-  }
-}
-
-/**
- * initialize migration
- * @param {import('./migration').Context} context 
- */
-function _create(item, options) {
-  switch (item) {
-    case 'table': {
-      __validate(options, {
-        name: 'required|string',
-        columns: 'required|array',
-        primary_column: 'string',
-        engine: 'string',
-      });
-      if (!options.columns.find(c => c.is_primary_key === true)) {
-        throw new Error('Primary key is required.');
-      }
-      let opt = {
-        name: options.name,
-        columns: _renderColumns(options.columns),
-        engine: options.engine || 'InnoDB',
-      };
-      return {
-        sql: _render(actions.table.create, opt),
-        values: [],
-      };
-    }
-    case 'column': {
-      __validate(options, {
-        table: 'required|string',
-        name: 'required|string',
-        type: 'required|string',
-        length: 'number',
-        unsigned: 'boolean',
-        not_null: 'boolean',
-        default: 'string',
-        comment: 'string',
-        auto_increment: 'boolean',
-        is_primary_key: 'boolean',
-        is_uniq_index: 'boolean'
-      });
-      return {
-        sql: _renderColumn(options),
-        values: [],
-      };
-    }
-    case 'index': {
-      __validate(options, {
-        table_name: 'required|string',
-        index_name: 'required|string',
-        column_names: 'required|array',
-      });
-      let opt = {
-        table_name: options.table_name,
-        index_name: options.index_name,
-        column_names: options.column_names,
-      };
-      return {
-        sql: _render(actions.index.create, opt),
-        values: [],
-      };
-    }
-    case 'foreign_key': {
-      __validate(options, {
-        table_name: 'required|string',
-        foreign_key_name: 'required|string',
-        column_name: 'required|string',
-        foreign_table_name: 'required|string',
-        foreign_column_name: 'required|string',
-      });
-      let opt = {
-        table_name: options.table_name,
-        foreign_key_name: options.foreign_key_name,
-        column_name: options.column_name,
-        foreign_table_name: options.foreign_table_name,
-        foreign_column_name: options.foreign_column_name,
-      };
-      return {
-        sql: _render(actions.foreign_key.create, opt),
-        values: [],
-      };
-    }
-    default:
-      throw new Error(`Unknown migration operation create${_caml_case(item, true)}.`);
-  }
-}
-
-/**
- * initialize migration
- * @param {import('./migration').Context} context 
- */
-function _drop(item, options) {
-  switch (item) {
-    case 'table': {
-      __validate({ table_name: options }, {
-        table_name: 'required|string'
-      });
-      let opt = { table_name: options.table_name };
-      return {
-        sql: _render(actions.table.drop, opt),
-        values: [],
-      };
-    }
-    case 'column': {
-      __validate(options, {
-        table_name: 'required|string',
-        column_name: 'required|string'
-      });
-      let opt = {
-        table_name: options.table_name,
-        column_name: options.column_name
-      };
-      return {
-        sql: _render(actions.column.drop, opt),
-        values: [],
-      };
-    }
-    case 'index': {
-      __validate(options, {
-        table_name: 'required|string',
-        index_name: 'required|string'
-      });
-      let opt = {
-        table_name: options.table_name,
-        index_name: options.index_name
-      };
-      return {
-        sql: _render(actions.index.drop, opt),
-        values: [],
-      };
-    }
-    case 'foreign_key': {
-      __validate(options, {
-        table_name: 'required|string',
-        foreign_key_name: 'required|string'
-      });
-      let opt = {
-        table_name: options.table_name,
-        foreign_key_name: options.foreign_key_name
-      };
-      return {
-        sql: _render(actions.foreign_key.drop, opt),
-        values: [],
-      };
-    }
-    default:
-      throw new Error(`Unknown migration operation drop${_caml_case(item, true)}.`);
   }
 }
 
@@ -369,19 +112,25 @@ async function _exec(context, queries) {
   try {
     const files = Object.keys(queries);
     await _foreach(files, async (file) => {
-      if (context.action === 'up') {
-        const hasMigarated = await transaction.table(context.task_key)
-          .where('migration_key', context.task_key)
-          .where('filename', file)
-          .count();
+      const hasMigarated = await transaction.table(context.task_key)
+        .where('migration_key', context.task_key)
+        .where('filename', file)
+        .count();
+      if (context.action === 'up' && hasMigarated) {
         if (hasMigarated) {
           printer.yellow(`Migration file "${file}" has been migrated.`).println();
           return;
         }
+      } else if (context.action === 'down' && !hasMigarated) {
+        return;
       }
 
       const sqls = queries[file];
+      // debug.halt(sqls);
       await _foreach(sqls, async (query) => {
+        if (context.debug) {
+          debug.log(query.sql);
+        }
         await transaction.query(query);
       });
 
@@ -391,6 +140,7 @@ async function _exec(context, queries) {
           filename: file,
           created_at: new Date()
         });
+        printer.green('Success migrate up: ').yellow(file).println(' ');
       } else {
         const item = await transaction.table(context.task_key)
           .where('migration_key', context.task_key)
@@ -398,6 +148,7 @@ async function _exec(context, queries) {
         if (item) {
           await transaction.table(context.task_key).where('id', item.id).delete();
         }
+        printer.green('Success migrate down: ').yellow(file).println(' ');
       }
     });
     await transaction.commit();
@@ -405,6 +156,118 @@ async function _exec(context, queries) {
     await transaction.rollback();
     debug.log(e);
   }
+}
+
+function _initMigration(file, queries = {}) {
+  const migration = {};
+  let baseAttr = {
+    writable: true,
+    enumerable: true,
+    configurable: true
+  };
+  Object.defineProperty(migration, 'createTable', {
+    value: function (table, columns, options = {}) {
+      _assign(options, {
+        operator: 'create',
+        target: 'table',
+        name: table,
+        columns
+      });
+      const builder = new ManageSQLBuilder(options);
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+  Object.defineProperty(migration, 'createColumn', {
+    value: function (name, type, table, options = {}) {
+      _assign(options, {
+        operator: 'create',
+        target: 'column',
+        table,
+        name,
+        type
+      });
+      const builder = new ManageSQLBuilder(options);
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+
+  Object.defineProperty(migration, 'createIndex', {
+    value: function (table, columns, options = {}) {
+      _assign(options, {
+        operator: 'create',
+        target: 'index',
+        name: options.indexName ? options.indexName : 'idx_' + table + '_' + columns.join('_'),
+        table,
+        columns
+      });
+      const builder = new ManageSQLBuilder(options);
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+
+  Object.defineProperty(migration, 'createForeignKey', {
+    value: function (options = {}) {
+      _assign(options, {
+        operator: 'create',
+        target: 'foreignKey',
+        name: options.foreignKey ? options.foreignKey : 'fk_' + options.tableName + '_' + options.columnName,
+        table: options.tableName,
+        column: options.columnName,
+        reference: options.reference
+      });
+      const builder = new ManageSQLBuilder(options);
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+
+  Object.defineProperty(migration, 'dropTable', {
+    value: function (table) {
+      const builder = new ManageSQLBuilder({
+        operator: 'drop',
+        target: 'table',
+        name: table
+      });
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+
+  Object.defineProperty(migration, 'dropColumn', {
+    value: function (name, table) {
+      const builder = new ManageSQLBuilder({
+        operator: 'drop',
+        target: 'column',
+        name,
+        table
+      });
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+
+  Object.defineProperty(migration, 'dropIndex', {
+    value: function (name, table) {
+      const builder = new ManageSQLBuilder({
+        operator: 'drop',
+        target: 'index',
+        name,
+        table
+      });
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+
+  Object.defineProperty(migration, 'dropForeignKey', {
+    value: function (name, table) {
+      const builder = new ManageSQLBuilder({
+        operator: 'drop',
+        target: 'foreignKey',
+        name,
+        table
+      });
+      queries[file].push({ sql: builder.sql, values: builder.values });
+    }, ...baseAttr
+  });
+
+  return migration;
 }
 
 /**
@@ -419,28 +282,7 @@ async function run(context) {
     const script = require(scriptPath);
     queries[file] = [];
 
-    const migration = {};
-
-    context.items.forEach((item) => {
-      Object.defineProperty(migration, 'create' + _caml_case(item, true), {
-        value: function (options) {
-          let query = _create.call(this, item, options);
-          queries[file].push(query);
-        },
-        writable: true,
-        enumerable: true,
-        configurable: true
-      });
-      Object.defineProperty(migration, 'drop' + _caml_case(item, true), {
-        value: function (options) {
-          let query = _drop.call(this, item, options);
-          queries[file].push(query);
-        },
-        writable: true,
-        enumerable: true,
-        configurable: true
-      });
-    });
+    const migration = _initMigration(file, queries);
 
     switch (context.action) {
       case 'up': {
@@ -468,7 +310,7 @@ async function run(context) {
 }
 
 async function end(context) {
-
+  printer.yellow('Done').println(' ');
 }
 
 module.exports = {
