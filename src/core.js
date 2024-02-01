8f6e62cddc80dc6a51dc1ebf82dd42070f4d7ef6
axiosleo <axiosleo@foxmail.com>
2024-02-01 18:11:56 +0800

    refactor: code to use core module for query handling

---
+++ src/core.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const { Builder } = require('./builder');
+
+/**
+ * 
+ * @param {import('mysql2/promise').Connection} conn 
+ * @param {*} options 
+ * @param {*} opt 
+ * @returns 
+ */
+const _query = (conn, options, opt = null) => {
+  switch (options.driver) {
+    case 'mysql': {
+      if (opt === null) {
+        const builder = new Builder(options);
+        opt = {
+          sql: builder.sql,
+          values: builder.values || [],
+        };
+      }
+      return new Promise((resolve, reject) => {
+        if (options.transaction) {
+          conn.execute(opt.sql, opt.values || []).then((res) => {
+            resolve(res[0]);
+          }).catch((err) => reject(err));
+          return;
+        }
+        conn.query(opt, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+
+      });
+    }
+    default: {
+      if (typeof options.queryHandler === 'function') {
+        const promise = options.queryHandler(conn, options, opt);
+        if (promise instanceof Promise) {
+          return promise;
+        }
+      }
+      throw new Error('queryHandler must return a promise');
+    }
+  }
+};
+
+const _execSQL = (conn, sql, values = []) => {
+  let opt = { sql, values };
+  return new Promise((resolve, reject) => {
+    if (conn.query instanceof Function) {
+      conn.query(opt, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    } else {
+      conn.execute(opt)
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
+    }
+  });
+};
+
+module.exports = {
+  _query,
+  _execSQL
+};
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Validator = require('validatorjs');
-const { Builder } = require('./builder');
 
 const _validate = (obj, rules, throwError = true) => {
   let validation = new Validator(obj, rules);
@@ -17,73 +16,6 @@ const _validate = (obj, rules, throwError = true) => {
   return null;
 };
 
-const _execSQL = (conn, sql, values = []) => {
-  let opt = { sql, values };
-  return new Promise((resolve, reject) => {
-    if (conn.query instanceof Function) {
-      conn.query(opt, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    } else {
-      conn.execute(opt)
-        .then((res) => resolve(res))
-        .catch((err) => reject(err));
-    }
-  });
-};
-
-/**
- * 
- * @param {import('mysql2/promise').Connection} conn 
- * @param {*} options 
- * @param {*} opt 
- * @returns 
- */
-const _query = (conn, options, opt = null) => {
-  switch (options.driver) {
-    case 'mysql': {
-      if (opt === null) {
-        const builder = new Builder(options);
-        opt = {
-          sql: builder.sql,
-          values: builder.values || [],
-        };
-      }
-      return new Promise((resolve, reject) => {
-        if (options.transaction) {
-          conn.execute(opt.sql, opt.values || []).then((res) => {
-            resolve(res[0]);
-          }).catch((err) => reject(err));
-          return;
-        }
-        conn.query(opt, (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-
-      });
-    }
-    default: {
-      if (typeof options.queryHandler === 'function') {
-        const promise = options.queryHandler(conn, options, opt);
-        if (promise instanceof Promise) {
-          return promise;
-        }
-      }
-      throw new Error('queryHandler must return a promise');
-    }
-  }
-};
-
 module.exports = {
-  _query,
-  _execSQL,
   _validate,
 };
