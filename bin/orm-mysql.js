242b157145b535b938b1517bbe85d78da6147616
axiosleo <axiosleo@foxmail.com>
2024-02-08 16:43:30 +0800

    Bump 0.9.3
    attr supported subQuery

--- bin/orm-mysql.js
+++ bin/orm-mysql.js
@@ -9,7 +9,7 @@ const app = new App({
   name: 'MySQL ORM CLI',
   desc: 'migrate, model, seed, etc.',
   bin: 'orm-mysql',
-  version: '0.9.2',
+  version: '0.9.3',
   commands_dir: path.join(__dirname, '../commands'),
 });
 
