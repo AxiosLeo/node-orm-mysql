daebd4c94f4d21438da2d89c083b2ccaf7ed5394
axiosleo <axiosleo@foxmail.com>
2026-02-23 17:57:33 +0800

    Bump 0.13.1

--- bin/orm-mysql.js
+++ bin/orm-mysql.js
@@ -9,7 +9,7 @@ const app = new App({
   name: 'MySQL ORM CLI',
   desc: 'migrate, model, seed, etc.',
   bin: 'orm-mysql',
-  version: '0.13.0',
+  version: '0.13.1',
   commands_dir: path.join(__dirname, '../commands'),
 });
 
